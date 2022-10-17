860edd399521ba69be05f3d258fbebdee9c51a4b
Matthew Marcus <matt.marcus@glu.com>
2022-10-16 22:36:41 -0700

    Update group column

--- database/migrations/2022_05_07_200320_create_teams_table.php
+++ database/migrations/2022_05_07_200320_create_teams_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedSmallInteger('group');
+            $table->unsignedTinyInteger('group');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2022_10_17_053235_create_teams_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::update('teams', function (Blueprint $table) {
+            $table->unsignedSmallInteger('group')->change();
+        });
+    }
+};
