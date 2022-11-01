42a0b1de097035297f0ffab7b6a6bcfc8cc8d0fc
Matthew Marcus <matt.marcus@glu.com>
2022-10-31 21:31:02 -0700

    Nicer score page

--- resources/views/fixtures/show.blade.php
+++ resources/views/fixtures/show.blade.php
@@ -15,7 +15,34 @@
         </div>
     @endif
 
-    {{ $fixture }}
+    <br/><br/>
+
+    <h1>{{ $fixture->teamOne()->first()->name }} vs. {{ $fixture->teamTwo()->first()->name }}</h1>
+
+    <br/>
+
+    @foreach (unserialize($fixture->history) as $event)
+        @if ($event[1] == 20)
+            @switch($event[0])
+                @case("t1p1")
+                <div>{{ $t1p1 }} ({{$fixture->teamOne()->first()->name}}) buzzed in for 20</div>
+                @break
+                @case("t2p1")
+                <div>{{ $t2p1 }} buzzed in for 20</div>
+                @break
+            @endswitch
+        @endif
+        @if ($event[0] == "t1bonuses")
+            <div>{{ $fixture->teamOne()->first()->name . " answered a tossup for " . $event[1] }}</div>
+        @endif
+        @if ($event[0] == "t2bonuses")
+            <div>{{ $fixture->teamTwo()->first()->name . " answered a tossup for " . $event[1] }}</div>
+        @endif
+    @endforeach
+
+    <br/>
+
+    <h1>Final score: {{ $t1_total }} - {{ $t2_total }}</h1>
 
 @endsection
 
